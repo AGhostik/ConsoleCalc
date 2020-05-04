6e4f632cb26c1caf217490ce8506844cd72a74b1
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2020-05-04 18:19:07 +0500

    refactor tests to StringExtensionsTests

---
+++ Tests/StringExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleCalc.Extensions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void NormalizeInput_AddSpacebars_DoNotHaveSpacebars()
+        {
+            var input = "1+5";
+            var result = input.AddSpacebars();
+            Assert.AreEqual("1 + 5", result);
+        }
+
+        [Test]
+        public void NormalizeInput_AddSpacebars_RemoveExcessLeadingSigns_DoNotHaveSpacebars()
+        {
+            var input = "1---5";
+            var result = input.RemoveExcessLeadingSign().AddSpacebars();
+            Assert.AreEqual("1 - 5", result);
+        }
+
+        [Test]
+        public void NormalizeInput_RemoveExcessLeadingSigns_HaveEvenNumberOfLeadingSign()
+        {
+            var input = "1 + ----5";
+            var result = input.RemoveExcessLeadingSign();
+            Assert.AreEqual("1 + 5", result);
+        }
+
+        [Test]
+        public void NormalizeInput_RemoveExcessLeadingSigns_HaveOddNumberOfLeadingSign()
+        {
+            var input = "1 + ---5";
+            var result = input.RemoveExcessLeadingSign();
+            Assert.AreEqual("1 + -5", result);
+        }
+
+        [Test]
+        public void NormalizeInput_RemoveExcessSpacebar_HaveMultipleSpacebar()
+        {
+            var input = "1 +     5";
+            var result = input.RemoveExcessSpacebar();
+            Assert.AreEqual("1 + 5", result);
+        }
+    }
+}
