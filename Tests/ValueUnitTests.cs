6092fe02d279c17d00dc0197d148b2e428aa16c5
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2020-05-04 16:52:13 +0500

    add valueUnit tests

---
+++ Tests/ValueUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleCalc;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ValueUnitTests
+    {
+        [Test]
+        public void TryParse_Integer()
+        {
+            var input = "5";
+            var result = ValueUnit.TryParse(input, out var valueUnit);
+            Assert.IsTrue(result);
+            Assert.AreEqual(5, valueUnit.GetResult());
+        }
+
+        [Test]
+        public void TryParse_NegativeInteger()
+        {
+            var input = "-5";
+            var result = ValueUnit.TryParse(input, out var valueUnit);
+            Assert.IsTrue(result);
+            Assert.AreEqual(-5, valueUnit.GetResult());
+        }
+
+        [Test]
+        public void TryParse_Decimal()
+        {
+            var input = "1.3";
+            var result = ValueUnit.TryParse(input, out var valueUnit);
+            Assert.IsTrue(result);
+            Assert.AreEqual(1.3, valueUnit.GetResult());
+        }
+
+        [Test]
+        public void TryParse_NegativeDecimal()
+        {
+            var input = "-1.3";
+            var result = ValueUnit.TryParse(input, out var valueUnit);
+            Assert.IsTrue(result);
+            Assert.AreEqual(-1.3, valueUnit.GetResult());
+        }
+    }
+}
