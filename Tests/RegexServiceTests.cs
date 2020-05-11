4d596b1a0b8a02c0352c515ae623292696585871
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2020-05-11 10:53:51 +0500

    refactor RegexServiceTests

--- Tests/RegexServiceTests.cs
+++ Tests/RegexServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ConsoleCalc;
+﻿using ConsoleCalc;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,145 +6,57 @@ namespace Tests
     [TestFixture]
     public class RegexServiceTests
     {
-        [Test]
-        public void FindMultipleSpacebars()
+        [TestCase("a          b", ExpectedResult = "          ")]
+        public string FindMultipleSpacebars(string input)
         {
             var regex = RegexService.GetRegex_FindMultipleSpacebars();
-            var spacebars = "          ";
-            var input = $"a{spacebars}b";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual(spacebars, matches[0].Value);
+            var match = regex.Match(input);
+            return match.Value;
         }
         
-        [Test]
-        public void FindMultipleMinusSymbol()
+        [TestCase("--a-b", ExpectedResult = "--")]
+        [TestCase("a------b", ExpectedResult = "------")]
+        public string FindMultipleMinusSymbol(string input)
         {
             var regex = RegexService.GetRegex_FindMultipleMinusSymbol();
-            var minuses1 = "--";
-            var minuses2 = "-------";
-            var input = $"{minuses1}a{minuses2}b";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(2, matches.Count);
-            Assert.AreEqual(minuses1, matches[0].Value);
-            Assert.AreEqual(minuses2, matches[1].Value);
+            var match = regex.Match(input);
+            return match.Value;
         }
 
-        [Test]
-        public void FindExpressionParts_ValuePlusValue()
+        [TestCase("1 + 2", ExpectedResult = "1")]
+        [TestCase("50 % 221", ExpectedResult = "50")]
+        [TestCase("(1 * 3.1) - (2.5 / 4.01)", ExpectedResult = "(1 * 3.1)")]
+        [TestCase("3.9 / (2 / 4)", ExpectedResult = "3.9")]
+        [TestCase("(1000 * 3) * 4321.1", ExpectedResult = "(1000 * 3)")]
+        public string FindExpressionParts_FirstValue(string input)
         {
             var regex = RegexService.GetRegex_FindExpressionParts();
-            var input = "1 + 2";
             var groups = regex.Match(input).Groups;
-            
-            Assert.AreEqual("1", groups["firstValue"].Value);
-            Assert.AreEqual("+", groups["operation"].Value);
-            Assert.AreEqual("2", groups["secondValue"].Value);
+            return groups["firstValue"].Value;
         }
 
-        [Test]
-        public void FindExpressionParts_ValueDivRemValue()
+        [TestCase("1 + 2", ExpectedResult = "2")]
+        [TestCase("50 % 221", ExpectedResult = "221")]
+        [TestCase("(1 * 3.1) - (2.5 / 4.01)", ExpectedResult = "(2.5 / 4.01)")]
+        [TestCase("3.9 / (2 / 4)", ExpectedResult = "(2 / 4)")]
+        [TestCase("(1000 * 3) * 4321.1", ExpectedResult = "4321.1")]
+        public string FindExpressionParts_SecondValue(string input)
         {
             var regex = RegexService.GetRegex_FindExpressionParts();
-            var input = "50 % 221";
             var groups = regex.Match(input).Groups;
-            
-            Assert.AreEqual("50", groups["firstValue"].Value);
-            Assert.AreEqual("%", groups["operation"].Value);
-            Assert.AreEqual("221", groups["secondValue"].Value);
+            return groups["secondValue"].Value;
         }
 
-        [Test]
-        public void FindExpressionParts_ExpressionMinusExpression()
-        {
-            var regex = RegexService.GetRegex_FindExpressionParts();
-            var input = "(1 * 3.1) - (2.5 / 4.01)";
-            var groups = regex.Match(input).Groups;
-            
-            Assert.AreEqual("(1 * 3.1)", groups["firstValue"].Value);
-            Assert.AreEqual("-", groups["operation"].Value);
-            Assert.AreEqual("(2.5 / 4.01)", groups["secondValue"].Value);
-        }
 
-        [Test]
-        public void FindExpressionParts_ValueDivExpression()
-        {
-            var regex = RegexService.GetRegex_FindExpressionParts();
-            var input = "3.9 / (2 / 4)";
-            var groups = regex.Match(input).Groups;
-            
-            Assert.AreEqual("3.9", groups["firstValue"].Value);
-            Assert.AreEqual("/", groups["operation"].Value);
-            Assert.AreEqual("(2 / 4)", groups["secondValue"].Value);
-        }
-
-        [Test]
-        public void FindExpressionParts_ExpressionMultiplyValue()
-        {
-            var regex = RegexService.GetRegex_FindExpressionParts();
-            var input = "(1000 * 3) * 4321.1";
-            var groups = regex.Match(input).Groups;
-            
-            Assert.AreEqual("(1000 * 3)", groups["firstValue"].Value);
-            Assert.AreEqual("*", groups["operation"].Value);
-            Assert.AreEqual("4321.1", groups["secondValue"].Value);
-        }
-
-        [Test]
-        public void FindExpessionForBracersAdding_FindMultiplyExpression()
+        [TestCase("10 * 3 + 1", ExpectedResult = "10 * 3")]
+        [TestCase("10.1 * 3.66 + 1", ExpectedResult = "10.1 * 3.66")]
+        [TestCase("13 + 66 * \"0\"", ExpectedResult = "66 * \"0\"")]
+        [TestCase("13 + \"1\" * \"2\"", ExpectedResult = "\"1\" * \"2\"")]
+        public string FindExpessionForBracersAdding(string input)
         {
             var regex = RegexService.GetRegex_FindExpessionForBracersAdding();
-            var input = "10 * 3 + 1";
             var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual("10 * 3", matches[0].Value);
-        }
-
-        [Test]
-        public void FindExpessionForBracersAdding_FindMultiplyExpression_Decimal()
-        {
-            var regex = RegexService.GetRegex_FindExpessionForBracersAdding();
-            var input = "10.1 * 3.66 + 1";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual("10.1 * 3.66", matches[0].Value);
-        }
-
-        [Test]
-        public void FindExpessionForBracersAdding_FindPlusExpression()
-        {
-            var regex = RegexService.GetRegex_FindExpessionForBracersAdding(false);
-            var input = "10 * 3 + 1";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual("3 + 1", matches[0].Value);
-        }
-
-        [Test]
-        public void FindExpessionForBracersAdding_FindMultiplyExpression_OneValueReplaced()
-        {
-            var regex = RegexService.GetRegex_FindExpessionForBracersAdding();
-            var input = "13 + 66 * \"0\"";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual("66 * \"0\"", matches[0].Value);
-        }
-
-        [Test]
-        public void FindExpessionForBracersAdding_FindPlusExpression_OneValueReplaced()
-        {
-            var regex = RegexService.GetRegex_FindExpessionForBracersAdding(false);
-            var input = "13 + \"1\" * \"2\"";
-            var matches = regex.Matches(input);
-
-            Assert.AreEqual(1, matches.Count);
-            Assert.AreEqual("13 + \"1\"", matches[0].Value);
+            return matches[0].Value;
         }
     }
 }
